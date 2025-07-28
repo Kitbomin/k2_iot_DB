dae2a3b9cb0e7f340955682179062f401b988854
Kitbomin <zzxxcc1433@naver.com>
2025-07-28 11:53:15 +0900

    기초

---
+++ b_/b_SQL명령어종류.sql
@@ -0,0 +1,33 @@
+-- b SQL 기초 명령어 종류
+
+### 명령어 종류
+
+# SQL Structured Query Language
+# : DB에서 데이터를 정의하고 조작하며 제어하기 위해 사용하는 표준 언어
+# - 데이터베이스 구조 정의
+# - 데이터를 조회, 삽입, 수정, 삭제
+# - 사용자 권한 제어
+
+# SQL 구성요소
+# DBMS에서 실행할 수 있는 명령의 종류에 따라
+#  DDL, DML, DCL로 분류
+
+# 1. DDL (Data Definition Language) : 데이터 정의어
+# - 데이터 베이스 스키마와 구조를 정의하는 데 사용
+# - CREATE, DROP, ALTER, TRUNCATE
+
+# 2. DML (Data Manipulation Language): 데이터 조작어
+# - 테이블에 데이터 검색, 등록, 수정, 삭제하는 데 사용됨
+# - SELECT, INSERT, UPDATE, DELETE
+
+# 3. DCL(Data Control Language) : 데이터 제어어
+# - 데이터 베이스의 사용자 권한을 관리하는 데에 사용됨
+# - GRANT, REVOKE
+
+# SQL 특징
+# - 비절차적 언어
+# 	: 결과를 '무엇을 원하는지(what) 중심' 으로 작성됨
+# 	> '어떻게 가져오는지(how)'는 DBMS 시스템 자체가 처리함
+# - 표준화
+# 	: ANSI에 의해 표준화 되어있음 | 대부분 DBMS에서 사용 가능
+
